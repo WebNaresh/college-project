78b9cfd2851a9b1ca1a43dc1a3d0a84289cc84de
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-04-10 15:34:22 +0530

    fixed-bat-scripts

--- start_nextjs.bat
+++ start_nextjs.bat
@@ -1,2 +1,11 @@
-cd "D:\Naresh-College-Project"
-npm run dev
+@echo off
+
+REM Check if anything is listening on port 3001
+netstat -ano | findstr :3001 > nul
+if errorlevel 1 (
+  echo Nothing is listening on port 3001. Starting Next.js application...
+  cd /d "D:\Naresh-College-Project"
+  npm run dev
+) else (
+  echo Something is already listening on port 3001. Exiting...
+)
